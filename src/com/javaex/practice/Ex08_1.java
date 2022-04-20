86120255bae3075f72abe78c6700a637d0590ee7
garden <dalkomjjang@gmail.com>
2022-04-20 17:51:44 +0900

    220420 주석

---
+++ src/com/javaex/practice/Ex08_1.java
@@ -0,0 +1,32 @@
+package com.javaex.practice;
+
+public class Ex08_1 {
+
+	public static void main(String[] args) {
+		
+		int[] LottoNums = new int[6];
+		int random = 0;
+		int count;
+		
+		
+		for (int i = 0; i < LottoNums.length; i++) { // for문 한 번 돌 떄 숫자 하나 생성 + 프린트
+			
+			count = 0;
+			while (true) { // 겹치지 않는 숫자 얻었을 때 break됨
+				random = (int)(Math.random()*45) + 1; // 난수 생성
+				
+				for (int j = 0; j < i; j++) {
+					if (LottoNums[j] != random) { // 기존 배열에 n이 없다면 count + 1
+						count += 1; // 중복되면 굳이 끝까지 확인 안함
+						break;
+					}
+				}
+				if (count == i) { // i-1까지의 수를 확인, index가 아니라 개수 체크이므로 +1
+					break; // 조건 만족하면 겹치지 않는 수이므로 break, 아니라면 다시 수 생성
+				}
+			}
+			LottoNums[i] = random;
+			System.out.print(random + " ");
+		}
+	}
+}
